2b7088f232df4e3381286601da192d7296edb0e8
Bobby <robdiesel87@gmail.com>
2021-10-29 18:29:59 -0600

    Create boot project

---
+++ TrackSleep/src/main/java/com/skilldistillery/tracksleep/TrackSleepApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.tracksleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackSleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackSleepApplication.class, args);
+	}
+
+}
