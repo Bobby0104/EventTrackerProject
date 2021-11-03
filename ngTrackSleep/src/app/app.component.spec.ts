a4ecf9f8f7d0f7854e83de147902f6e32a6ead78
Bobby <robdiesel87@gmail.com>
2021-11-02 20:09:03 -0600

    Created Angular project

--- ngTrackSleep/src/app/app.component.spec.ts
+++ ngTrackSleep/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'trackSleep'`, () => {
+  it(`should have as title 'ngTrackSleep'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('trackSleep');
+    expect(app.title).toEqual('ngTrackSleep');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('trackSleep app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ngTrackSleep app is running!');
   });
 });
